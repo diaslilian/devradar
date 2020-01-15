4d97ff1d95bf56eeb74f7b8a9a6a2e5e0a59468f
diaslilian <lilian.dias25@gmail.com>
2020-01-15 19:52:05 -0300

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack:omnistack@omnistack-eb6zm.mongodb.net/week10?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
